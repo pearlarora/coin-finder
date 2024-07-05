3a9832a876848dd68cf9738c09325486de438a38
pearlarora <ipearlarora@gmail.com>
2024-07-05 20:34:42 +0530

    nodemailer plain error

--- src/Config/emailService.js
+++ src/Config/emailService.js
@@ -9,14 +9,15 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  authMethod: "PLAIN",
 });
 
-export const sendEmail = async (to, subject, body) => {
+export const sendEmail = async (to, subject, text) => {
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to,
     subject,
-    body,
+    text,
   };
 
   try {
