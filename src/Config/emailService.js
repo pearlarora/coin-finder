4407ebfef77da2c42ceadeb192b491027280d53e
pearlarora <ipearlarora@gmail.com>
2024-07-05 21:02:22 +0530

    nodemailer cred

--- src/Config/emailService.js
+++ src/Config/emailService.js
@@ -6,8 +6,10 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    // user: process.env.EMAIL_USER,
+    // pass: process.env.EMAIL_PASS,
+    user: "coinfinderlisting@gmail.com",
+    pass: "PearlSparsh02"
   },
   authMethod: "PLAIN",
 });
